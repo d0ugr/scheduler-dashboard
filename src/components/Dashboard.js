23eed8cb1ce07210fd18071297cb9831f447dad7
d0ugr <doug@escm.ml>
2020-04-25 13:44:27 -0400

    Add loading state

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,11 +3,17 @@ import React, { Component } from "react";
 import classnames from "classnames";
 
 class Dashboard extends Component {
+
+  state = {
+    loading: true
+  }
+
   render() {
     const dashboardClasses = classnames("dashboard");
 
     return <main className={dashboardClasses} />;
   }
+
 }
 
 export default Dashboard;
